29afb8456e9b2c56ea8a64920ba42584aa23dfb8
P4UL0HENR1QU3 <paulohenriqueooliveira.2021@gmail.com>
2025-03-24 16:51:11 -0300

    Alterações do dia 24/03

--- sistema/views.py
+++ sistema/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 
-# Aqui irão ficar todas as views (controladores) ref ao sistema
-
-
+# Aqui irão ficar todas as views (controladores) ref ao App sistema
+# A função index informa o que vai acontecer quando ela for chamada.
 def index(request):
     return render(
         request,
         'sistema/index.html',
     )
+
