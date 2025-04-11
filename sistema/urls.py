b3748a3226d9755e10aa1f6c347eeb9f279949c7
P4UL0HENR1QU3 <paulohenriqueooliveira.2021@gmail.com>
2025-04-11 16:37:45 -0300

    Aula do dia 11/04/2025 - Mostrando dados do BD no HTML

--- sistema/urls.py
+++ sistema/urls.py
@@ -4,5 +4,10 @@ from sistema import views
 # Informa qual será a rota que irá chamar determinada view (função).
 urlpatterns = [
     path('', views.index),
-    path('apresentacao/', views.apresentacao),
+    path('listar/', views.listarUsuarios),
+]
+
+urlpatterns = [
+    path('', views.index),
+    path('listar/', views.listarFilmes),
 ]
